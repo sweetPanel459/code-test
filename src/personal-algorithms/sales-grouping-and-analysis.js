c1e972eb40eca7cf6574620fff5ddc884c5a5b12
otpJs <sssantiago459@gmail.com>
2024-12-16 22:42:48 -0500

    deteccion de anomalias

--- src/personal-algorithms/sales-grouping-and-analysis.js
+++ src/personal-algorithms/sales-grouping-and-analysis.js
@@ -2,14 +2,23 @@ import { templateJson, inputJson } from "../constants/input-json-analysis.js";
 
 // main function
 
+//todo:
+//  1. la detecion de anomalias debe retornar el indice y los objetos anomalos
+//  2. creacion de la funcion de filtraciob
 const salesGroupingAndAnalysis = () => {
+  let monthlyListing = {};
+
   for (let i = 0; i < inputJson.length; i++) {
+    const wrongObj = [];
+
     const isValid = confirmObjectFormat(inputJson[i], templateJson);
+    const thereIsAnomalousData = anomalyDataDetection(inputJson[i], wrongObj);
+    const filteredObject = objectLeak(thereIsAnomalousData);
+
     if (!isValid) return;
   }
 
   const generalSummaryPart = generalSummary(inputJson);
-  console.log(generalSummaryPart);
 };
 
 // helpers functions
@@ -21,16 +30,20 @@ const confirmObjectFormat = (object, template) => {
   }
 };
 
-// TODO :
-// 1. Validacion de que la fecha no sea superior a la fetcha de hoy.
-// 2. Validacion de que la cantidad no sea negativa.
-// 3. Validacion de que el precio no sea negativo.
+const anomalyDataDetection = (object, wrongObj) => {
+  const validationRules = [
+    { conditional: object.Cantidad <= 0 },
+    { conditional: object.Precio_unitario <= 0 },
+  ];
 
-const AnomalyDataDetection = (object) => {
-  const validationRules = {
-    amount: object.Cantidad < 0,
-  };
+  for (const rule of validationRules) {
+    if (rule.conditional) wrongObj.push(object);
+  }
+
+  return anomalousObjects;
 };
+
+const objectLeak = () => { };
 
 const generalSummary = (object) => {
   let totalIncome = 0;
@@ -50,9 +63,6 @@ const generalSummary = (object) => {
   };
 };
 
-// utils functions
-
-const isFutureDate = () => { };
 // main call
 
 salesGroupingAndAnalysis();
